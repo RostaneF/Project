02c1a0e6f968c4c3d5b87f7db195c04218e6dec7
rostane <rostane.feknous@edu.devinci.fr>
2023-03-14 22:35:52 +0100

    : Actualisation des données

---
+++ script.sh
@@ -0,0 +1,7 @@
+curl "https://www.marketwatch.com/investing/index/spx" > /home/rostane/Projet_final/Project/SP500.html
+Value=$(cat /home/rostane/Projet_final/Project/SP500.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
+Date=$(date +%Y-%m-%d-%H:%M)
+echo "$Date;$Value" >> data.csv
+git add .
+git commit -m "$(Date) : Actualisation des données" 
+git push
