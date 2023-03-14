f986878cee0ba2304534d5b99a19da95a952015d
rostane <rostane.feknous@edu.devinci.fr>
2023-03-14 23:11:59 +0100

    2023-03-14:23h11 : Actualisation des données

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
-curl "https://www.marketwatch.com/investing/index/spx" > /home/rostane/Projet_final/Project/SP500.html
-curl "https://www.marketwatch.com/investing/index/ndx" > /home/rostane/Projet_final/Project/NASDAQ.html
-Value1=$(cat /home/rostane/Projet_final/Project/SP500.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
-Value2=$(cat /home/rostane/Projet_final/Project/NASDAQ.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
+curl "https://www.marketwatch.com/investing/index/spx" > /home/rostane/Projet_final/Project/Generated_data/SP500.html
+curl "https://www.marketwatch.com/investing/index/ndx" > /home/rostane/Projet_final/Project/Generated_data/NASDAQ.html
+Value1=$(cat /home/rostane/Projet_final/Project/Generated_data/SP500.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
+Value2=$(cat /home/rostane/Projet_final/Project/Generated_data/NASDAQ.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
 Date=$(date +%Y-%m-%d-%H:%M)
-echo "$Date;$Value1;$Value2" >> data.csv
+echo "$Date;$Value1;$Value2" >> /home/rostane/Projet_final/Project/Generated_data/data.csv
 git add .
 git commit -m "$(date -d "now" +"%Y-%m-%d:%Hh%M") : Actualisation des données" 
