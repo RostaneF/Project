08c339e0c3ca01ee830c75df2d55033b2558f392
rostane <rostane.feknous@edu.devinci.fr>
2023-03-14 23:08:41 +0100

    2023-03-14:23h08 : Actualisation des données

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 curl "https://www.marketwatch.com/investing/index/spx" > /home/rostane/Projet_final/Project/SP500.html
-Value=$(cat /home/rostane/Projet_final/Project/SP500.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
+curl "https://www.marketwatch.com/investing/index/ndx" > /home/rostane/Projet_final/Project/NASDAQ.html
+Value1=$(cat /home/rostane/Projet_final/Project/SP500.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
+Value2=$(cat /home/rostane/Projet_final/Project/NASDAQ.html | grep -oP '(?<=<span class="value">)\w+,\w+.\w+'| sed 's/,//g')
 Date=$(date +%Y-%m-%d-%H:%M)
-echo "$Date;$Value" >> data.csv
+echo "$Date;$Value1;$Value2" >> data.csv
 git add .
 git commit -m "$(date -d "now" +"%Y-%m-%d:%Hh%M") : Actualisation des données" 
