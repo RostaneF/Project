1034fd545fb4a67aa187f0f6a4dfbd15d0a5cdba
rostane <rostane.feknous@edu.devinci.fr>
2023-03-21 05:07:17 -0400

    Modification du code et actualisation des données

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Git_Linux_Python project
 
 Ceci est le repository associé au projet.
+
+Note importante : le main python est executé dans la session "Session_active" de tmux. Pour y acceder : tmux attach -t Session_active
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import time
 from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
 
 df = pd.read_csv("/home/ubuntu/Projet/Project/Generated_data/data.csv", delimiter=";")
 app = Dash(__name__)
@@ -13,19 +14,62 @@ app.layout = html.Div(children=[
     ]),
 
     dcc.Graph(
-        id='Cours des Index S&P 500',
+        id='Cours Index S&P 500',
         figure={
             'data': [
-                {'x': df['date'], 'y': df['SP500'],'type': 'line', 'name': 'SP500'},
-                {'x': df['date'], 'y': df['NASDAQ'], 'type': 'line', 'name': 'NASDAQ'},
-            ],
+                {'x': pd.to_datetime(df['date'], format='%Y-%m-%d-%H-%M'), 'y': df['SP500'],'type': 'area', 'name': 'SP500'},
+               ],
             'layout': {
-                'title': 'Evolution S&P500 et NASDAQ'
+                'title': 'Evolution de Idx S&P500'
             }
         }
+    ),
+    dcc.Graph(
+        id='Cours Index NASDAQ',
+        figure={
+            'data': [
+                {'x': pd.to_datetime(df['date'], format='%Y-%m-%d-%H-%M'), 'y': df['NASDAQ'],'type': 'area', 'name': 'NASDAQ'},
+               ],
+            'layout': {
+                'title': 'Evolution de Idx  NASDAQ'
+            }
+        }
+    ),
+    dcc.Interval(
+        id='interval-component',
+        interval=60*1000,
+        n_intervals=0
     )
 ])
+@app.callback(
+    [Output('Cours Index S&P 500', 'figure'),
+     Output('Cours Index NASDAQ', 'figure')],
+    Input('interval-component', 'n_intervals')
+)
+def update_all_graphs(n):
+    # Charger les nouvelles données depuis votre fichier CSV
+    df = pd.read_csv("/home/ubuntu/Projet/Project/Generated_data/data.csv", delimiter=";")
+    # Mettre à jour la figure du graphique SP500 avec les nouvelles données
+    sp500_figure = {
+        'data': [
+            {'x': pd.to_datetime(df['date'], format='%Y-%m-%d-%H-%M'), 'y': df['SP500'],'type': 'area', 'name': 'SP500'},
+        ],
+        'layout': {
+            'title': 'Evolution de Idx S&P500'
+        }
+    }
+    # Mettre à jour la figure du graphique NASDAQ avec les nouvelles données
+    nasdaq_figure = {
+        'data': [
+            {'x': pd.to_datetime(df['date'], format='%Y-%m-%d-%H-%M'), 'y': df['NASDAQ'],'type': 'area', 'name': 'NASDAQ'},
+        ],
+        'layout': {
+            'title': 'Evolution de Idx NASDAQ'
+        }
+    }
+    return [sp500_figure, nasdaq_figure]
+
 
 if __name__ == "__main__":
 	app.run_server(host = "0.0.0.0", port = 8050, debug = True)
-	time.sleep(300)
+	time.sleep(60)
